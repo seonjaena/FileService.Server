82b75f9a2654ab1e049c899ff80684258012f7af
나선재 <sky114z21@gmail.com>
2024-09-20 12:45:52 +0900

    build-docker.bat 파일 수정

--- build-docker.bat
+++ build-docker.bat
@@ -1,17 +1,8 @@
 @echo off
 
-gradlew.bat wrapper
-
-call gradlew.bat clean test
-set GRADLE_EXIT_CODE=%ERRORLEVEL%
-if %GRADLE_EXIT_CODE% neq 0 (
-    exit /b %GRADLE_EXIT_CODE%
-)
-
-for /f "tokens=2 delims=:" %%a in ('.\gradlew.bat properties ^| findstr "^version:"') do (
-    set "PROJECT_VER=%%a"
-)
-set "PROJECT_VER=%PROJECT_VER: =%"
+for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /format:list') do set "TIMESTAMP=%%I"
+set "TIMESTAMP=%TIMESTAMP:~0,14%"
 
 for /f "delims=" %%i in ('git symbolic-ref --short HEAD') do set "GIT_BRANCH=%%i"
-docker build --build-arg version=%PROJECT_VER% -t .server:%PROJECT_VER%-%GIT_BRANCH% .
+
+docker build -t file.server:%GIT_BRANCH%-%TIMESTAMP% .
